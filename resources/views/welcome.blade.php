ea81fb091839e3137fc9d3f44161a55aaf94602f
Eltoncht97 <elton.m.chavez@gmail.com>
2020-04-16 18:36:10 -0300

    First Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel de Elton
                 </div>
 
                 <div class="links">
